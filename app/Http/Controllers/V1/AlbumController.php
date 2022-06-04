2600ba8756e42a8074e5503f753fe43356252032
Sanil Shrestha <santonil2004@yahoo.com>
2022-06-05 01:58:23 +1000

    move album controller inside v1 namespace

---
+++ app/Http/Controllers/V1/AlbumController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Controllers\V1;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreAlbumRequest;
+use App\Http\Requests\UpdateAlbumRequest;
+use App\Models\Album;
+
+class AlbumController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Album::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param \App\Http\Requests\StoreAlbumRequest $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreAlbumRequest $request)
+    {
+        return Album::create($request->all());
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param \App\Models\Album $album
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Album $album)
+    {
+        return $album;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param \App\Http\Requests\UpdateAlbumRequest $request
+     * @param \App\Models\Album $album
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateAlbumRequest $request, Album $album)
+    {
+        $album->updateOrFail($request->all());
+        return $album;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param \App\Models\Album $album
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Album $album)
+    {
+        $album->deleteOrFail();
+        return response('', 204);
+    }
+}
---
+++ app/Http/Resources/V1/AlbumResource.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Resources\V1;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class AlbumResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        dd($request);
+        return parent::toArray($request);
+    }
+}
